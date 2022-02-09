63344ae8328f843c0eef556e8a6d322dce01d423
nth-block <20584285+nth-block@users.noreply.github.com>
2022-02-10 01:31:36 +0530

    Extract name from JWT in Authorization header

--- conf.d/logging.js
+++ conf.d/logging.js
@@ -1,7 +1,23 @@
 function kvAccess(r) {
     var log = `${r.variables.time_iso8601} client=${r.remoteAddress} method=${r.method} uri=${r.uri} status=${r.status}`;
-    r.rawHeadersIn.forEach(h => log += ` in.${h[0]}=${h[1]}`);
-    r.rawHeadersOut.forEach(h => log += ` out.${h[0]}=${h[1]}`);
+    // r.rawHeadersOut.forEach(h => log += ` out.${h[0]}=${h[1]}`);
+    r.rawHeadersIn.forEach(function(h) {
+        log += ` in.${h[0]}=${h[1]}`;
+        if(h[0] == "Authorization")
+        {
+            // Decode JWT. No signature verification as that will be done in the backend. Only claims extraction here
+            let jwt = h[1].split(" ")[1].split(".")[1]; // Extracting the claims part from the JWT
+            let jwtJson = Buffer.from(jwt, 'base64').toString();
+            let jwtClaims = JSON.parse(jwtJson);
+            log += " in:Name=" + jwtClaims.name;
+        }
+    });
+
+    if(false == log.includes("in.Authorization"))
+    {
+        log += " in.Authorization=No Authorization header passed";
+    }
+
     return log;
 }
 
