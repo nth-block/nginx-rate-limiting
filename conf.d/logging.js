637e83112e3ea2f87ccf323f3b8f13a227c7ea69
nth-block <20584285+nth-block@users.noreply.github.com>
2022-02-10 01:55:13 +0530

    Beautify what gets logged

--- conf.d/logging.js
+++ conf.d/logging.js
@@ -2,21 +2,18 @@ function kvAccess(r) {
     var log = `${r.variables.time_iso8601} client=${r.remoteAddress} method=${r.method} uri=${r.uri} status=${r.status}`;
     // r.rawHeadersOut.forEach(h => log += ` out.${h[0]}=${h[1]}`);
     r.rawHeadersIn.forEach(function(h) {
-        log += ` in.${h[0]}=${h[1]}`;
-        if(h[0] == "Authorization")
+        if(h[0] == "Authorization") // Not logging the authorization header as that will be a security risk
         {
             // Decode JWT. No signature verification as that will be done in the backend. Only claims extraction here
             let jwt = h[1].split(" ")[1].split(".")[1]; // Extracting the claims part from the JWT
             let jwtJson = Buffer.from(jwt, 'base64').toString();
             let jwtClaims = JSON.parse(jwtJson);
-            log += " in:Name=" + jwtClaims.name;
+            log += " in:Authorization.SubjectName=" + jwtClaims.name;
+        }
+        else {
+            log += ` in.${h[0]}=${h[1]}`;
         }
     });
-
-    if(false == log.includes("in.Authorization"))
-    {
-        log += " in.Authorization=No Authorization header passed";
-    }
 
     return log;
 }
